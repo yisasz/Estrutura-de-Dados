cee796eb121d27c23c17cfe50932144451cb0b67
Isabela Viana <155593544+IsavianaDev@users.noreply.github.com>
2025-02-11 20:21:43 -0300

    Add files via upload

---
+++ Lista1/Enigma/Main.java.java
@@ -0,0 +1,30 @@
+package executavel;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+
+		String dados = scanner.next();
+
+		String palavraChave = scanner.next();
+
+		for (int i = 0; i < palavraChave.length(); i++) {
+			char letra = palavraChave.charAt(i);
+			int index = dados.indexOf(letra);
+			if (index != -1) {
+				dados = dados.substring(0, index) + dados.substring(index + 1);
+			} else {
+				System.out.println("Nenhuma informacao util");
+				return;
+			}
+		}
+
+		System.out.println("Palavra chave encontrada");
+
+		scanner.close();
+	}
+}
