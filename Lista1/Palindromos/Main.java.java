0203b874e1637a7ce339fb6aedfaa6639dfb0365
Isabela Viana <155593544+IsavianaDev@users.noreply.github.com>
2025-02-11 23:01:24 -0300

    Add files via upload

---
+++ Lista1/Palindromos/Main.java.java
@@ -0,0 +1,26 @@
+package executavel;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String palavra = sc.nextLine();
+
+		if (palavra.length() >= 1 && palavra.length() <= 100) {
+			char[] vetor = palavra.toCharArray();
+
+			for (int i = 0, j = vetor.length - 1; i < j; i++, j--) {
+				if (vetor[i] != vetor[j]) {
+					System.out.println("Não");
+					return;
+				}
+			}
+			System.out.println("Sim");
+		}
+
+	}
+
+}
