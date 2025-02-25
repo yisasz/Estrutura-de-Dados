12534e84c9870ae60180a1e0a146feee48f05724
Isabela Viana <155593544+IsavianaDev@users.noreply.github.com>
2025-02-25 17:45:08 -0300

    Add files via upload

---
+++ Lista2/Fatorial/Fatorial.java.java
@@ -0,0 +1,25 @@
+package executavel;
+
+import java.util.Scanner;
+
+public class Fatorial {
+
+	public static int fat(int n) {
+
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+
+		return n * fat(n - 1);
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+
+		System.out.println(fat(n));
+
+		scanner.close();
+	}
+}
---
+++ Lista2/Fibonacci/Fibonacci.java.java
@@ -0,0 +1,27 @@
+package executavel;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static int fibonacci(int n) {
+
+		if (n == 0) {
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		}
+
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+
+		System.out.println(fibonacci(n));
+
+		scanner.close();
+	}
+}
